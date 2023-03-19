773a2e78c155bacf0ace9d7112f8cbd04a664436
mslipenchuk267 <mslipenchuk267@gmail.com>
2023-03-19 13:10:46 -0400

    Added Money, CommonSense, and BookSmarts params to Agent. Updated ToString

--- src/cmd/ca-agent-modeling.go
+++ src/cmd/ca-agent-modeling.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	my2dAgent := &agent.Agent{Coords: []int{1,2}, Activation: 1, Suggestibility: 0.5}
-	my3dAgent := &agent.Agent{Coords: []int{1,2,3}, Activation: 0, Suggestibility: 0.1}
+	my2dAgent := &agent.Agent{Coords: []int{1,2}, Activation: 1, Money: 1000, CommonSense: 0.6, BookSmarts: 0.7, Suggestibility: 0.5}
+	my3dAgent := &agent.Agent{Coords: []int{1,2,3}, Activation: 0, Money: 15000, CommonSense: 0.2, BookSmarts: 0.9, Suggestibility: 0.1}
 	
 	fmt.Println("Printing Agent Info on 2d and 3d coords")
 	my2dAgent.ToString()
